bc7a046b5209998ab4ce1102da3034ad10079d2a
Nikesh001 <nikkataria74@gmail.com>
2017-10-11 15:05:06 +0000

    Fix ril for DU

--- device.mk
+++ device.mk
@@ -243,8 +243,9 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
-    libcnefeatureconfig \
-    libxml2
+    libxml2 \
+    libprotobuf-cpp-full \
+    rild_socket
 
 # Sensors
 PRODUCT_COPY_FILES += \
