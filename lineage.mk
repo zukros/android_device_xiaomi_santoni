82448caa4cc4161aacb9419a7f5b1f23fad5d093
Akshay Sankle <32200419+zukros@users.noreply.github.com>
2018-02-03 19:07:21 +0530

    Device related info added

--- lineage.mk
+++ lineage.mk
@@ -19,8 +19,14 @@ $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_santoni
 BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := santoni
+PRODUCT_NAME := lineage_santoni
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi 4X
+TARGET_VENDOR := Xiaomi
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
